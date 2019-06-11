902442616f5da693f7ad8d660fb30c206b693322
tuzmusic <tuzmusic@gmail.com>
2019-06-11 17:43:57 -0400

    handle search success in reducer

--- __tests__/CustomerSearch.test.js
+++ __tests__/CustomerSearch.test.js
@@ -24,11 +24,15 @@ beforeEach(() => jest.resetAllMocks());
 const text = "55-57 59th St";
 const searchField = "address";
 const searchParams = { text, searchField, customers };
+const startAction = { type: "SEARCH_CUSTOMERS_START", searchParams };
+const successAction = {
+  type: "CUSTOMER_SEARCH_SUCCESS",
+  results: [customers[0]]
+};
 
 describe("searchCustomers", () => {
   it("kicks off a redux action to start the search", () => {
-    const expectedAction = { type: "SEARCH_CUSTOMERS_START", searchParams };
-    expect(searchCustomers(searchParams)).toEqual(expectedAction);
+    expect(searchCustomers(searchParams)).toEqual(startAction);
   });
 });
 
@@ -36,11 +40,7 @@ describe("searchSaga", () => {
   const initialAction = { type: "SEARCH_CUSTOMERS_START", searchParams };
   it("searches customers with a saga", async () => {
     const dispatched = await recordSaga(searchSaga, initialAction);
-    const expectedAction = {
-      type: "CUSTOMER_SEARCH_SUCCESS",
-      results: [customers[0]]
-    };
-    expect(dispatched).toContainEqual(expectedAction);
+    expect(dispatched).toContainEqual(successAction);
   });
 
   it("returns an error on a failure", async () => {
@@ -51,10 +51,10 @@ describe("searchSaga", () => {
     const expectedAction = { type: "CUSTOMER_SEARCH_FAILURE", error };
     try {
       const dispatched = await recordSaga(searchSaga, initialAction);
-      console.log("***DISPATCHED FROM TRY IN TEST***", dispatched[0].type);
+      // console.log("***DISPATCHED FROM TRY IN TEST***", dispatched[0].type);
       expect(dispatched).toContainEqual(expectedAction);
     } catch (e) {
-      console.log("***ERROR FROM CATCH IN TEST***", e.message);
+      // console.log("***ERROR FROM CATCH IN TEST***", e.message);
     }
     spy.mockRestore();
   });
@@ -65,5 +65,13 @@ describe("searchApi", () => {
     const apiResult = searchApi(searchParams);
     const customersFromResult = apiResult.map(c => new Customer.fromApi(c));
     expect(customersFromResult).toContainEqual(customers[0]);
+  });
+});
+import reducer from "../src/redux/reducers/customerReducer";
+describe("reducer", () => {
+  it("takes a search success action and returns the state with the results", () => {
+    expect(reducer(undefined, successAction).searchResults).toEqual(
+      successAction.results
+    );
   });
 });
--- src/redux/reducers/customerReducer.js
+++ src/redux/reducers/customerReducer.js
@@ -1,13 +1,18 @@
+// @flow
+
 import Customer from "../../models/Customer";
 import customers from "../../../__mocks__/customers";
 
 const initialState = {
   customers,
-  currentCustomer: customers[0]
+  currentCustomer: customers[0],
+  searchResults: []
 };
 
 export default function customerReducer(state = initialState, action) {
   switch (action.type) {
+    case "CUSTOMER_SEARCH_SUCCESS":
+      return { ...state, searchResults: action.results };
     default:
       return state;
   }
