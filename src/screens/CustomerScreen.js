cc2c5dbc647335faa16f523ebb63a8f3564ec314
tuzmusic <tuzmusic@gmail.com>
2019-06-12 11:07:33 -0400

    move review button back into customerscreen file

--- src/screens/CustomerScreen.js
+++ src/screens/CustomerScreen.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { StyleSheet, ScrollView, View } from "react-native";
-import { ThemeProvider, Text, Divider, Rating } from "react-native-elements";
+import {
+  ThemeProvider,
+  Text,
+  Divider,
+  Rating,
+  Button
+} from "react-native-elements";
 import Customer from "../models/Customer";
-import ReviewForm from "../subviews/ReviewForm";
 import Review from "../subviews/ReviewView";
 import NewReviewScreen from "./NewReviewScreen";
 
@@ -14,10 +19,8 @@ export class CustomerScreen extends Component {
       <ThemeProvider theme={theme}>
         <ScrollView contentContainerStyle={styles.container}>
           <CustomerInfo customer={customer} />
-          {/*  <Divider style={styles.divider} />
-          <Text h2>Reviews</Text>
-          <ReviewsList reviews={customer.reviews} />
-          <ReviewForm /> */}
+          <ReviewsList customer={customer} />
+          <ReviewButton />
           <NewReviewScreen />
         </ScrollView>
       </ThemeProvider>
@@ -34,23 +37,33 @@ const CustomerInfo = ({ customer }) => {
       <Text style={styles.detailText}>{customer.address}</Text>
       <Text style={styles.detailText}>{customer.phone}</Text>
       <Text style={styles.detailText}>{customer.email}</Text>
-      <Text style={styles.detailText}> </Text>
     </View>
   );
 };
 
-export const ReviewsList = ({ reviews }) => {
+const ReviewButton = props => {
+  return (
+    <Button
+      title="Leave a Review"
+      style={styles.button}
+      containerStyle={styles.buttonContainer}
+    />
+  );
+};
+
+export const ReviewsList = ({ customer }) => {
+  const { reviews, averageRating } = customer;
   return (
     <View>
-      <Text style={styles.detailText}>
-        Rating ({customer.reviews.length} reviews):{" "}
-      </Text>
+      <Text style={styles.detailText}>Rating ({reviews.length} reviews): </Text>
       <Rating
         readonly
         startingValue={customer.averageRating}
         style={styles.rating}
         imageSize={20}
       />
+      <Text style={styles.detailText}>{/* blank line */} </Text>
+      <Text h2>Reviews</Text>
       {reviews.map((review, i) => (
         <Review review={review} key={i} />
       ))}
@@ -66,6 +79,11 @@ const styles = {
     marginRight: 20,
     justifyContent: "flex-start",
     alignItems: "flex-start"
+  },
+  buttonContainer: {
+    alignItems: "center",
+    paddingTop: 20,
+    width: "100%"
   },
   divider: {
     backgroundColor: "black",
