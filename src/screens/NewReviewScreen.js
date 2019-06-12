8f2a15f6136e4f1334e45c36fed87acb694c8b24
tuzmusic <tuzmusic@gmail.com>
2019-06-12 10:42:12 -0400

    review view

--- src/screens/NewReviewScreen.js
+++ src/screens/NewReviewScreen.js
@@ -1,35 +1,51 @@
 import React, { Component } from "react";
 import { Text, Input, Button, AirbnbRating } from "react-native-elements";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 export default class NewReviewScreen extends Component {
   state = {
     text:
       "If true, the text input can be multiple lines. The default value is false. It is important to note that this aligns the text to the top on iOS, and centers it on Android. Use with textAlignVertical set to top for the same behavior in both platforms.",
-    text: ""
+    text: "",
+    rating: 5
   };
-
+  rate(rating) {}
+  handleSubmit() {
+    console.log(this.state);
+  }
   render() {
     return (
       <View style={styles.container}>
-        <AirbnbRating defaultRating={5} />
+        <View style={styles.ratingContainer}>
+          <Text style={styles.ratingLabel}>Rating</Text>
+          <AirbnbRating
+            defaultRating={this.state.rating}
+            showRating={false}
+            onFinishRating={rating => this.setState({ rating })}
+            imageSize={10}
+          />
+        </View>
         <Input
+          ref={ref => (this.textInput = ref)}
+          label={"Review"}
           inputStyle={styles.input}
           inputContainerStyle={styles.inputContainer}
           clearButtonMode={"while-editing"}
-          containerStyle={{ padding: 0 }}
           placeholder={"Enter your review"}
           value={this.state.text}
           onChangeText={text => this.setState({ text })}
           multiline={true}
           textAlignVertical={"top"}
           numberOfLines={100}
+          labelStyle={styles.ratingLabel}
         />
-        <Button title="Submit" />
+        <Button title="Submit" onPress={this.handleSubmit.bind(this)} />
       </View>
     );
   }
 }
+
+const border = { borderColor: "blue", borderWidth: 0.5 };
 
 const styles = {
   container: {
@@ -50,5 +66,13 @@ const styles = {
     borderBottomWidth: 0,
     marginVertical: 15,
     marginHorizontal: 5
+  },
+  ratingContainer: { width: "100%", paddingHorizontal: 10, marginBottom: 15 },
+  ratingLabel: {
+    textAlign: "left",
+    alignItems: "flex-start",
+    color: "grey",
+    fontWeight: "bold",
+    fontSize: 16
   }
 };
